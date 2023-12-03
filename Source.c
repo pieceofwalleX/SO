90f7f79db7b4b90a6cf2e957fdc5245bc3e5e329
notmyfault <fallenangelhd06@gmail.com>
2023-12-03 16:30:01 +0000

    GET CPU LOAD

--- Source.c
+++ Source.c
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include <string.h>
 
+#define COMMAND_ERROR_1 "Command not found!"
+
 /*
     Funcao usada para criar
 */
@@ -14,8 +16,24 @@ void create_file(){
 /*
     Funcao usada para ler o input
 */
-void read_command(char **argv[]){
+void read_command(int argc,char **argv[]){
+    //Caso o 1 argumento seja top vamos redirecionalo
+    if(!strcmp(argv[1],"top")){
+        if(argc > 2){
+            fprintf(stderr,"%s\n",COMMAND_ERROR_1);
+            fprintf(stderr,"Command top format: mycmd top\n");
+            return EXIT_FAILURE;
+        }
 
+        double cpuload[3];
+
+        if(getloadavg(cpuload,3) == -1){
+            fprintf(stderr,"Error getting cpu load");
+            return EXIT_FAILURE;
+        }
+
+        fprintf(stdout,"CPU LOAD: %.2f %.2f %.2f",cpuload[0],cpuload[1],cpuload[3]);
+    }
 }
 /*
     Funcao usada para executar o comando desejado pelo utilizador
@@ -23,6 +41,9 @@ void read_command(char **argv[]){
 void execute_command(char** argv[]){
 
 }
+/*
+    Funcao usada quando o argumento top e escolhido
+*/
 
 int main(int argc, char *argv[]){
 
@@ -30,10 +51,10 @@ int main(int argc, char *argv[]){
     if(argc < 2 ){
         fprintf(stderr,"Format: mycmd <...>\n");
         fprintf(stderr,"\nmycmd --help\n");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
-    read_command(argv);
+    read_command(argc,argv);
 
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
