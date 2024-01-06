54b1b497c1147f1157049c41363b47165c8b78b4
notmyfault <fallenangelhd06@gmail.com>
2024-01-06 21:42:00 +0000

    CHANGES To TOP

--- Source.c
+++ Source.c
@@ -161,102 +161,122 @@ int execute_multicommands(comands input)
                 dup2(pipe_controles[0], STDIN_FILENO);
                 close(pipe_controles[0]);
 
-                //Digitar --color=always para aparecer a vermelho
+                // Digitar --color=always para aparecer a vermelho
 
                 execvp(input.argv_cmd2[0], input.argv_cmd2);
             }
         }
     }
-
-
 }
 
-int is_process(const char* name){
-    for(size_t i = 0; i < strlen(name); i++){
-        if(name[i] < '0' || name[i] > '9'){
+int is_process(const char *name)
+{
+    for (size_t i = 0; i < strlen(name); i++)
+    {
+        if (name[i] < '0' || name[i] > '9')
+        {
             return 0;
         }
     }
     return 1;
 }
 
-int number_of_process(){
+int number_of_process()
+{
 
     DIR *dir;
     struct dirent *entry;
 
     dir = opendir("/proc");
 
-    if(dir == NULL){
-        fprintf(stderr,"Falha ao abir /proc");
+    if (dir == NULL)
+    {
+        fprintf(stderr, "Falha ao abir /proc");
         return 1;
     }
 
     int count = 0;
 
-    while((entry = readdir(dir)) != NULL){
-        if(is_process(entry->d_name)){
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (is_process(entry->d_name))
+        {
             count++;
         }
     }
     return count;
 }
 
-void running_process(){
+void running_process()
+{
     DIR *dir = opendir("/proc/");
     struct dirent *entry;
     struct stat file_info;
-    char nome[200],status[200],user[200];
+    char nome[200], status[200], user[200];
 
     int i = 0;
 
-    if(dir == NULL){
-        fprintf(stderr,"Falha ao abir /proc");
+    if (dir == NULL)
+    {
+        fprintf(stderr, "Falha ao abir /proc");
         return 1;
     }
-    fprintf(stdout,"####################################################################################\n");
-    fprintf(stdout,"@ID\t\t\t@STATUS\t\t\t\t@NAME\n");
-    fprintf(stdout,"####################################################################################\n\n");
-    while((entry = readdir(dir)) != NULL){
+    fprintf(stdout, "####################################################################################\n");
+    fprintf(stdout, "@ID\t\t\t@STATUS\t\t\t\t@NAME\n");
+    fprintf(stdout, "####################################################################################\n\n");
+    while ((entry = readdir(dir)) != NULL)
+    {
         char path[300];
-        snprintf(path,sizeof(path),"/proc/%s",entry->d_name);
+        snprintf(path, sizeof(path), "/proc/%s", entry->d_name);
 
-        //Verificar se e um diretorio
-        if(!stat(path,&file_info) && S_ISDIR(file_info.st_mode)){
+        // Verificar se e um diretorio
+        if (!stat(path, &file_info) && S_ISDIR(file_info.st_mode))
+        {
             int pid = atoi(entry->d_name);
 
-            if(pid > 0){
+            if (pid > 0)
+            {
                 char status[300];
-                snprintf(status,sizeof(status),"/proc/%d/status",pid);
+                snprintf(status, sizeof(status), "/proc/%d/status", pid);
 
-                FILE *fp = fopen(status,"r");
-                
+                FILE *fp = fopen(status, "r");
 
-
-                if(fp != NULL){ 
+                if (fp != NULL)
+                {
                     char line[300];
-                    while(fgets(line,sizeof(line),fp)){
-                        if(strstr(line,"Name:")){
-                            sscanf(line,"Name: %[^\n]",nome);
-                        }else if(strstr(line,"State:")){
-                            sscanf(line,"State: %[^\n]",status);
-                        }else if(strstr(line,"User:")){
-                            sscanf(line,"User: %[^\n]",user);
+                    while (fgets(line, sizeof(line), fp))
+                    {
+                        if (strstr(line, "Name:"))
+                        {
+                            sscanf(line, "Name: %[^\n]", nome);
+                        }
+                        else if (strstr(line, "State:"))
+                        {
+                            sscanf(line, "State: %[^\n]", status);
+                        }
+                        else if (strstr(line, "User:"))
+                        {
+                            sscanf(line, "Uid: %*d %50s", user);
                         }
                     }
-                }else{
+                }
+                else
+                {
                     // continue;
                 }
-                fprintf(stdout,"%d",pid);
-                fprintf(stdout,"\t\t\t%s",status);
-                fprintf(stdout,"\t\t\t/proc/%d/status%s\n",pid,nome);
-                fclose(fp);
-                
-                if(i % 20 == 0){
-                    sleep(10);
-                    system("clear");
+                if (!strcmp(status, "R (running)"))
+                {
+                    fprintf(stdout, "%d", pid);
+                    fprintf(stdout, "\t\t\t%s", status);
+                    fprintf(stdout, "\t\t\t%s", user);
+                    fprintf(stdout, "\t\t\t/proc/%d/status%s\n", pid, nome);
+                    fclose(fp);
+                    i++;
                 }
-                i++;
+                else
+                {
+                    continue;
+                }
             }
         }
     }
@@ -267,32 +287,37 @@ void running_process(){
 */
 int command_top()
 {
+    char c;
+    while (1)
+    {   
 
-    double cpuload[3];
-    FILE *load = fopen("/proc/loadavg", "r");
+        double cpuload[3];
+        FILE *load = fopen("/proc/loadavg", "r");
 
-    if (load != NULL)
-    {
-        if ((fscanf(load, "%lf %lf %lf", &cpuload[0], &cpuload[1], &cpuload[2])) != 3)
+        if (load != NULL)
         {
-            fprintf(stderr, "Error getting cpu load");
+            if ((fscanf(load, "%lf %lf %lf", &cpuload[0], &cpuload[1], &cpuload[2])) != 3)
+            {
+                fprintf(stderr, "Error getting cpu load");
+                return 1;
+            }
+
+            fprintf(stdout, "CPU LOAD: %.2f %.2f %.2f\n", cpuload[0], cpuload[1], cpuload[2]);
+        }
+        else
+        {
+            fprintf(stderr, "Error opening /proc/loadavg\n");
             return 1;
         }
 
-        fprintf(stdout, "CPU LOAD: %.2f %.2f %.2f\n", cpuload[0], cpuload[1], cpuload[2]);
+        fprintf(stdout, "Processes : %d\n", number_of_process());
+        running_process();
 
 
+        sleep(10);
+        system("clear");
     }
-    else
-    {
-        fprintf(stderr, "Error opening /proc/loadavg\n");
-        return 1;
-    }
-
-    fprintf(stdout,"Processes : %d\n",number_of_process());
-    running_process();
     return 0;
-
 }
 
 int main(int argc, char *argv[])
