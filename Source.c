b1fb112954c5f54a3f09a89fe5250ed5b984169d
notmyfault <fallenangelhd06@gmail.com>
2024-01-06 03:28:48 +0000

    SOME FIXES

--- Source.c
+++ Source.c
@@ -9,7 +9,8 @@
 
 #define COMMAND_ERROR_1 "Command not found!"
 
-typedef struct {
+typedef struct
+{
     int found;
     const char *operator;
     int argc_cmd1;
@@ -18,60 +19,70 @@ typedef struct {
     char **argv_cmd2;
 } comands;
 
-comands parse(char *argv[], int argc) {
+comands parse(char *argv[], int argc)
+{
     comands result;
 
-    result.found = 0; 
-    result.operator = NULL;
+    result.found = 0;
+    result.operator= NULL;
     result.argc_cmd1 = 0;
     result.argc_cmd2 = 0;
     result.argv_cmd1 = NULL;
     result.argv_cmd2 = NULL;
 
     int found = 0;
-    for (int i = 0; i < argc; i++) {
-        if (strcmp(argv[i], ">") == 0 || strcmp(argv[i], "<") == 0 || strcmp(argv[i], "|") == 0) {
+    for (int i = 0; i < argc; i++)
+    {
+        if (strcmp(argv[i], ">") == 0 || strcmp(argv[i], "<") == 0 || strcmp(argv[i], "|") == 0)
+        {
             found = 1;
             result.found = 1;
-	    result.operator =  argv[i];
+            result.operator= argv[i];
             continue;
         }
 
-        if(!found) {
+        if (!found)
+        {
             // Adiciona argumentos no contexto do cmd1
             result.argv_cmd1 = realloc(result.argv_cmd1, (result.argc_cmd1 + 1) * sizeof(char *));
             result.argv_cmd1[result.argc_cmd1] = strdup(argv[i]);
             result.argc_cmd1++;
-        } else {
-            // Adiciona argumentos no contexto do cmd2 
+        }
+        else
+        {
+            // Adiciona argumentos no contexto do cmd2
             result.argv_cmd2 = realloc(result.argv_cmd2, (result.argc_cmd2 + 1) * sizeof(char *));
             result.argv_cmd2[result.argc_cmd2] = strdup(argv[i]);
             result.argc_cmd2++;
         }
     }
     result.argv_cmd1[result.argc_cmd1] = NULL;
-    if (found) result.argv_cmd2[result.argc_cmd2] = NULL;
-
+    if (found)
+        result.argv_cmd2[result.argc_cmd2] = NULL;
 
     return result;
 }
 /*
     Funcao usada para criar ficheiro
 */
-void create_file(){
-
+void create_file()
+{
 }
 
 /*
     Funcao usada para ler o input
 */
-void read_command(int argc,char *argv[]){
+void read_command(int argc, char *argv[])
+{
 
-    //Caso o 1 argumento seja top vamos redirecionalo
-    if(!strcmp(argv[1],"top")){
+    // Caso o 1 argumento seja top vamos redirecionalo
+    if (!strcmp(argv[1], "top"))
+    {
         command_top();
-    }else{
-        execute_command(parse(argv,argc));
+    }
+    else
+    {
+        execute_command(parse(argv, argc));
     }
 
     return;
@@ -79,13 +90,17 @@ void read_command(int argc,char *argv[]){
 /*
     Funcao usada para executar o comando desejado pelo utilizador
 */
-int execute_command(comands input){
-    char ** subarguments;
+int execute_command(comands input)
+{
+    char **subarguments;
 
-    if(!input.found){
+    if (!input.found)
+    {
         subarguments = input.argv_cmd1 + 1;
-        execvp(input.argv_cmd1[1],subarguments);
-    }else{
+        execvp(input.argv_cmd1[1], subarguments);
+    }
+    else
+    {
 
         execute_multicommands(input);
 
@@ -95,8 +110,9 @@ int execute_command(comands input){
     return 0;
 }
 
-int execute_multicommands(comands input){
-    char ** subarguments;
+int execute_multicommands(comands input)
+{
+    char **subarguments;
     /*
         Aqui basicamente vai se criar um processo filho para executar
         os argumentos antes dos operadores >,< ou |
@@ -106,82 +122,83 @@ int execute_multicommands(comands input){
     int pipe_controles[2];
     pid_t child;
 
-    if(pipe(pipe_controles) == -1){
-        fprintf(stderr,"Falha ao criar o PIPE");
+    if (pipe(pipe_controles) == -1)
+    {
+        fprintf(stderr, "Falha ao criar o PIPE");
         return 1;
     }
 
     child = fork();
 
-    if(child == -1){
-        fprintf(stderr,"Falha ao criar Processo");
+    if (child == -1)
+    {
+        fprintf(stderr, "Falha ao criar Processo");
         return 1;
     }
 
-    if(!strcmp(input.operator,"|")){
-        if(child == 0){
-            /*
-                Fechar a leitura de dados no processo filho
-            */
-            close(pipe_controles[0]);
-            dup2(pipe_controles[1],STDOUT_FILENO);
-            close(pipe_controles[1]);
+    if (child == 0)
+    {
+        /*
+            Fechar a leitura de dados no processo filho
+        */
+        close(pipe_controles[0]);
+        dup2(pipe_controles[1], STDOUT_FILENO);
+        close(pipe_controles[1]);
 
-            subarguments = input.argv_cmd1 + 1;
+        subarguments = input.argv_cmd1 + 1;
 
-            execvp(input.argv_cmd1[1],subarguments);
-        }else{
-            /*
-                Fechar a escrita de dados no processo pai
-            */
-            close(pipe_controles[0]);
-            dup2(pipe_controles[0],STDIN_FILENO);
-            close(pipe_controles[0]);
-
-            
-
-            subarguments = input.argv_cmd2;
-            execvp(input.argv_cmd2[0],subarguments);
-        }
-        waitpid(child,NULL,0);
+        execvp(input.argv_cmd1[1], subarguments);
+    }
+    else
+    {
+        /*
+            Fechar a escrita de dados no processo pai
+        */
+        close(pipe_controles[1]);
+        dup2(pipe_controles[0], STDIN_FILENO);
+        close(pipe_controles[0]);
+        execvp(input.argv_cmd2[0], input.argv_cmd2);
     }
 }
 
 /*
     Funcao usada quando o argumento top e escolhido
 */
-int command_top(){
+int command_top()
+{
 
     double cpuload[3];
-    FILE *file = fopen("/proc/loadavg","r");
-    
+    FILE *file = fopen("/proc/loadavg", "r");
 
-    if(file != NULL){
-        if((fscanf(file,"%lf %lf %lf",&cpuload[0],&cpuload[1],&cpuload[2]) )!= 3){
-            fprintf(stderr,"Error getting cpu load");
+    if (file != NULL)
+    {
+        if ((fscanf(file, "%lf %lf %lf", &cpuload[0], &cpuload[1], &cpuload[2])) != 3)
+        {
+            fprintf(stderr, "Error getting cpu load");
             return 1;
         }
 
-        fprintf(stdout,"CPU LOAD: %.2f %.2f %.2f\n",cpuload[0],cpuload[1],cpuload[2]);
+        fprintf(stdout, "CPU LOAD: %.2f %.2f %.2f\n", cpuload[0], cpuload[1], cpuload[2]);
         return 0;
-    }else{
-        fprintf(stderr,"Error opening /proc/loadavg\n");
+    }
+    else
+    {
+        fprintf(stderr, "Error opening /proc/loadavg\n");
         return 1;
     }
 }
 
+int main(int argc, char *argv[])
+{
 
-
-int main(int argc, char *argv[]){
-
-
-    if(argc < 2 ){
-        fprintf(stderr,"Format: mycmd <...>\n");
-        fprintf(stderr,"\nmycmd --help\n");
+    if (argc < 2)
+    {
+        fprintf(stderr, "Format: mycmd <...>\n");
+        fprintf(stderr, "\nmycmd --help\n");
         return EXIT_FAILURE;
     }
 
-    read_command(argc,argv);
+    read_command(argc, argv);
 
     return EXIT_SUCCESS;
 }
