a06a94b95ec810966844cf5154676a158cbc416c
notmyfault <fallenangelhd06@gmail.com>
2024-01-06 01:21:08 +0000

    CHANGES

--- Source.c
+++ Source.c
@@ -81,7 +81,10 @@ void read_command(int argc,char *argv[]){
 */
 int execute_command(comands input){
     if(!input.found){
-        execvp(input.argv_cmd1[0],input.argv_cmd1);
+        if(execlp(input.argv_cmd1[0],input.argv_cmd1[1],NULL) == -1){
+            perror("Comando nao encontrado");
+            return 1;
+        }
     }
     return 0;
 }
